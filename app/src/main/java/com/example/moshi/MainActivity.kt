f02deafa6bafa3473b80fb05700de2c2045f1f5a
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-11-07 20:48:48 +0900

    ++

--- app/src/main/java/com/example/moshi/MainActivity.kt
+++ app/src/main/java/com/example/moshi/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.moshi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moshi.databinding.ActivityMainBinding
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        // JSON 데이터
+        val json = "{\"name\":\"John Doe\",\"age\":30,\"isDeveloper\":true}"
+
+        // Moshi 인스턴스 생성
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build();
+
+        // Person 클래스와 연결된 JsonAdapter 생성
+        val personAdapter: JsonAdapter<Person> = moshi.adapter(Person::class.java);
+
+        // JSON을 Person 객체로 파싱
+        val person: Person? = personAdapter.fromJson(json);
+
+        // 파싱된 결과를 출력
+        if (person != null) {
+            binding.tv1.text = "${person.name}";
+            binding.tv2.text = "${person.age}";
+            binding.tv3.text = "${person.isDeveloper}";
+            println("Name: ${person.name}");
+            println("Age: ${person.age}");
+            println("Is Developer: ${person.isDeveloper}");
+        } else {
+            println("JSON 파싱 실패");
+        };
+
     }
 }
---
+++ app/src/main/java/com/example/moshi/Person.kt
@@ -0,0 +1,3 @@
+package com.example.moshi
+
+data class Person (val name: String, val age: Int, val isDeveloper: Boolean);
